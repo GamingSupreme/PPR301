ef6c13a76a6ffca96da8c4020f73e14b79621919
Josh <joshualeite2003@gmail.com>
2023-06-16 00:10:13 +0800

    Movement Changes
    Made movement feel a bit smoother, not happy with the speeds the character can get up to tho so will need to manually limit the max speed of the player

--- PPR301/Assets/Scripts/Movement.cs
+++ PPR301/Assets/Scripts/Movement.cs
@@ -7,6 +7,18 @@ public class Movement : MonoBehaviour
     //Set player move speed
     public float moveSpeed;
 
+    //Set Players height
+    public float playerHeight;
+
+    //Determine what is ground and what isnt
+    public LayerMask whatIsGround;
+
+    //Used for checking whether the player can jump or not
+    bool grounded;
+
+    //To help simulate proper movement
+    public float groundDrag;
+
     //Reference players orientation
     public Transform orientation;
 
@@ -22,15 +34,26 @@ public class Movement : MonoBehaviour
 
     private void Start(){
         //On scene start we reference this objects rigid body to out variable
-        // and freeze its rotation
+        //and freeze its rotation
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
     }
 
     private void Update()
     {
+        //Check if the player is touching the ground
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f, whatIsGround);
+
         //Make sure to be checking for player input on every update frame
         PlayerInput();
+
+        //Check if the player is touching the ground, if they are apply drag
+        //Otherwise dont
+        if (grounded) {
+            rb.drag = groundDrag;
+        }else{
+            rb.drag = 0;
+        }  
     }
 
     private void FixedUpdate()
@@ -50,6 +73,7 @@ public class Movement : MonoBehaviour
         //In the cam script
         inputDir = orientation.forward* verticalInput + orientation.right * horizontalInput;
 
+        //adds force in the direction the player is facing
         rb.AddForce(inputDir.normalized * moveSpeed * 10f, ForceMode.Force);
     }
 
