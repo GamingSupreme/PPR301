8adaada6c55c42bc16ea63f2ab98bf86bea30d27
Rockcopper3290 <matthewpettett307@gmail.com>
2023-06-25 20:49:21 +1000

    Health just about done

--- PPR301/Assets/Scripts/UI Scripts/PauseMenu.cs
+++ PPR301/Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -7,13 +7,15 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
-    //public GameObject healthMenuUI;
+    public GameObject healthUI;
 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        //if the player presses down on the ESC or P key, it will only take the first frame of that key press and then pause the game
+        // or unpause the game if the game is already paused
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
         {
             if (GameIsPaused)
             {
@@ -29,18 +31,22 @@ public class PauseMenu : MonoBehaviour
 
     void Resume()
     {
+        //closes the heathUI and brings up the menuUI 
         pauseMenuUI.SetActive(false);
-        //healthMenuUI.SetActive(true);
+        healthUI.SetActive(true);
 
+        // resumes the game speed back to normal
         Time.timeScale = 1;
         GameIsPaused = false;
     }
 
     void Pause()
     {
+        //closes the menuUI and brings up the heathUI
         pauseMenuUI.SetActive(true);
-        //healthMenuUI.SetActive(false);
+        healthUI.SetActive(false);
 
+        // freezes the game and sets bool to say that the game is paused
         Time.timeScale = 0;
         GameIsPaused = true;
     }
@@ -53,6 +59,8 @@ public class PauseMenu : MonoBehaviour
     public void OnClick_QuitGame()
     {
         Debug.Log("Game has been closed");
+        //quits the game
+        Application.Quit();
     }
 
 }
