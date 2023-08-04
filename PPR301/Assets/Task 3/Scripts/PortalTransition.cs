e84ba8331a2790966db08bbf61bbf7cec3312951
Josh <joshualeite2003@gmail.com>
2023-08-04 13:57:32 +0800

    Portal Transition
    Now when the player collects all 5 collectables and walks within the bounds of the portal it will activate and when they walk into the portal theyll be transported to a finish level screen

---
+++ PPR301/Assets/Task 3/Scripts/PortalTransition.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalTransition : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision){
+        if (collision.gameObject.tag == "portalPlane"){
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+
+}
