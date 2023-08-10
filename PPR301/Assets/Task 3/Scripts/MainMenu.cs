8d5d6d53627776ac9b4f78995360c2fcb7eaab47
Rockcopper3290 <matthewpettett307@gmail.com>
2023-08-11 00:19:57 +1000

    Start menu done with End menu 80% sorted

---
+++ PPR301/Assets/Task 3/Scripts/MainMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Pannels")]
+    public GameObject StartPannel;
+    public GameObject OptionsPannel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Vertical_Slice_map");
+    }
+
+    public void OptionsMenu()
+    {
+        //Set options menu to true
+        //set start pannel to false
+    }
+
+    public void ExitGame()
+    {
+        // Quits the game
+        QuitGame();
+    }
+
+    public void Update()
+    {
+        // When the escape key is pressed -> exit game
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Quits the game
+            QuitGame();
+        }
+    }
+
+
+    //=====================================================================================================================
+    //=====================================================================================================================
+
+    // Functions
+
+    void QuitGame()
+    {
+        Debug.Log("Game has been closed");
+        Application.Quit();
+    }
+    
+}
