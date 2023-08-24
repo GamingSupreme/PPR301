817c0cd3465a83011a049e7f7034a02b7571357b
Rockcopper3290 <matthewpettett307@gmail.com>
2023-08-25 01:10:31 +1000

    Plan B implimented (Raycast detection for aimers)

--- PPR301/Assets/Task 3/Scripts/RedicalChanger.cs
+++ PPR301/Assets/Task 3/Scripts/RedicalChanger.cs
@@ -6,21 +6,58 @@ using UnityEngine.UIElements;
 
 public class RedicalChanger : MonoBehaviour
 {
-    public GameObject reticalTarget;
-    private void OnTriggerEnter(Collider other)
-    {
-        reticalTarget.SetActive(true);
+    public float CastRange = 5f;
+    public GameObject UI_Aimer;
 
-        Debug.Log("ENTER");
-        //check to 
+    public void Update()
+    {
+        Vector3 direction = Vector3.forward;
+        Ray theRay = new Ray(transform.position, transform.TransformDirection(direction * CastRange));
+        
+        //draw ray for us to see in scene screen
+        Debug.DrawRay(transform.position, transform.TransformDirection(direction * CastRange));
+
+        if (Physics.Raycast(theRay, out RaycastHit hit, CastRange))
+        {
+            if (hit.collider.tag == "Untagged")
+            {
+                UI_Aimer.SetActive(true);
+                print("its a grappleable object");
+            }
+        }
+        else
+        {
+            UI_Aimer.SetActive(false);
+        }
     }
 
-
-    private void OnTriggerExit(Collider other)
-    {
-        reticalTarget.SetActive(false);
-
-        Debug.Log("EXIT");
-
-    }
 }
+
+    /*
+    //public GameObject reticalTarget;
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    //if (other.gameObject.layer == 6)
+    //    //{
+    //        reticalTarget.SetActive(true);
+    //    //}
+    //    Debug.Log("ENTER");
+    //    //check to 
+    //}
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    // other.gameObject.layer == 6 &&
+    //    if (reticalTarget.activeInHierarchy == false)
+    //    {
+    //        reticalTarget.SetActive(true);
+    //    }
+    //    Debug.Log("STAY");
+    //}
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    reticalTarget.SetActive(false);
+
+    //    Debug.Log("EXIT");
+
+    //}
+    */
